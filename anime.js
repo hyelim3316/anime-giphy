871b2263410d4c94664bf9794968f8f5b54496fa
hyelim3316@gmail.com <hyelim3316@gmail.com>
2019-03-19 23:52:39 -0400

    second

--- anime.js
+++ anime.js
@@ -1,39 +1,30 @@
-var topic = ["totoro", "kiki", "mononoke"];
+var topic = ["totoro", "kiki", "mononoke", "sakura", "ponyo" ];
 
 
 $("button").on("click", function () {
-    // Grabbing and storing the data-anime property value from the button
+
     var anime = $(this).attr("data-anime");
 
-    // Constructing a queryURL using the anime name
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         anime + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-    // Performing an AJAX request with the queryURL
     $.ajax({
         url: queryURL,
         method: "GET"
     })
-        // After data comes back from the request
         .then(function (response) {
 
-            // storing the data from the AJAX request in the results variable
             var results = response.data;
             console.log(results);
 
-            // Looping through each result item
             for (var i = 0; i < results.length; i++) {
 
-                // Creating and storing a div tag
                 var animeDiv = $("<div>");
 
-                // Creating a paragraph tag with the result item's rating
                 var p = $("<p>").text("Rating: " + results[i].rating);
 
-                // Creating and storing an image tag
                 var animeImage = $("<img>");
 
-                // Setting the src attribute of the image to a property pulled off the result item
                 animeImage.attr("src", results[i].images.fixed_height.url);
                 animeImage.attr("data-animate", results[i].images.fixed_height.url);
                 animeImage.attr("data-still", results[i].images.fixed_height_still.url);
@@ -42,11 +33,9 @@ $("button").on("click", function () {
 
 
 
-                // Appending the paragraph and image tag to the animalDiv
                 animeDiv.append(p);
                 animeDiv.append(animeImage);
 
-                // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
                 $("#gifs-appear-here").prepend(animeDiv);
             }
         });
